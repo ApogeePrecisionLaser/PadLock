c729d9ec63edd80766c0bb005e8d013ffd0e1406
dhruvil64 <dhruvil.apogee@gmail.com>
2020-05-22 11:58:56 +0530

    nordictest

---
+++ pca10040/s132/ses/Output/ble_app_blinky_pca10040_s132 Release/Obj/ses_startup_nrf_common.d
@@ -0,0 +1,2 @@
+Output/ble_app_blinky_pca10040_s132\ Release/Obj/ses_startup_nrf_common.o: \
+ C:\Users\Sai\Desktop\nRF5_SDK_16\modules\nrfx\mdk\ses_startup_nrf_common.s
---
+++ pca10040/s132/ses/Output/ble_app_blinky_pca10040_s132 Release/Obj/utf.d
@@ -0,0 +1,6 @@
+Output/ble_app_blinky_pca10040_s132\ Release/Obj/utf.o: \
+ C:\Users\Sai\Desktop\nRF5_SDK_16\external\utf_converter\utf.c \
+ C:\Users\Sai\Desktop\nRF5_SDK_16\external\utf_converter\utf.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.30c/include/stddef.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.30c/include/__crossworks.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.30c/include/stdint.h
